68b972b7bfd3d274ac0b76ab2b6acdfa8fd69ffd
Asadulloh <doniyornasirdinov82@gmail.com>
2025-11-10 16:31:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ button.onclick = function() {
 }
 
 let tugmaA = document.getElementById("tugmaA");
+let tugmaB = document.getElementById("tugmaB");
+let tugmaC = document.getElementById("tugmaC");
 
 tugmaA.onclick = function() {
     window.location.href = "moy.html";
+}
+tugmaB.onclick = function() {
+    window.location.href = "about.html";
+}
+tugmaC.onclick = function() {
+    window.location.href = "aloqa.html";
 }
